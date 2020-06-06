c596e043307a9d17cf5b5c948c90eedf78fd4963
Anca Simon <ancarsimon@gmail.com>
2020-06-06 14:59:29 -0500

    wip

--- src/components/shared/ItemCard/ItemCard.js
+++ src/components/shared/ItemCard/ItemCard.js
@@ -12,6 +12,8 @@ class ItemCard extends React.Component {
 
   render() {
     const { item } = this.props;
+    const singleLink = `/stuff/${item.id}`;
+    const editLink = `/edit/${item.id}`;
 
     return (
       <div className="ItemCard col-6">
@@ -21,6 +23,11 @@ class ItemCard extends React.Component {
           </div>
           <div class="card-body">
             <img src={item.itemImage} alt="stuff I hoard" className="stuffImage" />
+            <div className="row justify-content-center">
+              <Link className="btn btn-secondary itemButton" to={singleLink}>View</Link>
+              <Link className="btn btn-dark itemButton" to={editLink}>Edit</Link>
+              <button className="btn btn-danger itemButton">Delete</button>
+            </div>
           </div>
         </div>
       </div>
