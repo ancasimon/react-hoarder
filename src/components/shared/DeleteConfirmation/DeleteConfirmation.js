b2673568ce4ded5225a5483e7070cfe9a5250e98
Anca Simon <ancarsimon@gmail.com>
2020-06-09 20:35:47 -0500

    added delete conf messages to both single view and My StTuff page

---
+++ src/components/shared/DeleteConfirmation/DeleteConfirmation.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Swal from 'sweetalert2';
+
+import './DeleteConfirmation.scss';
+
+class DeleteConfirmation extends React.Component {
+  render() {
+    return (
+      <div className="DeleteConfirmation">
+        Swal.fire({
+          {
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+          }
+        })
+        {/* .then((result) => {
+          if (result.value) {
+            Swal.fire(
+              'Deleted!',
+              'Your file has been deleted.',
+              'success'
+            )
+          }
+        }) */}
+      </div>
+    );
+  }
+}
+
+export default DeleteConfirmation;
--- src/components/shared/ItemCard/ItemCard.js
+++ src/components/shared/ItemCard/ItemCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Swal from 'sweetalert2';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 
 import itemShape from '../../../helpers/propz/itemShape';
 
@@ -12,8 +14,35 @@ class ItemCard extends React.Component {
     removeItem: PropTypes.func.isRequired,
   }
 
+  deleteItem = (itemId) => {
+    const { removeItem } = this.props;
+    removeItem(itemId);
+  }
+
+  deleteConfirmation = () => {
+    const { item } = this.props;
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success',
+          this.deleteItem(item.id),
+        );
+      }
+    });
+  }
+
   render() {
-    const { item, removeItem } = this.props;
+    const { item } = this.props;
     const singleLink = `/stuff/${item.id}`;
     const editLink = `/edit/${item.id}`;
 
@@ -28,7 +57,10 @@ class ItemCard extends React.Component {
             <div className="row justify-content-center">
               <Link className="btn btn-secondary itemButton" to={singleLink}>View</Link>
               <Link className="btn btn-dark itemButton" to={editLink}>Edit</Link>
-              <button className="btn btn-danger itemButton" onClick={() => removeItem(item.id)}>Delete</button>
+              <button className="btn btn-danger itemButton" onClick={ () => {
+                this.deleteConfirmation();
+              }
+              }>Delete</button>
             </div>
           </div>
         </div>
